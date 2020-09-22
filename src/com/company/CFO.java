6fa39577746fedb3a4d7d1e69fb3b81f1499db54
Ethan Knable <ethanknable99@gmail.com>
2020-09-22 11:54:29 -0400

    Added variables, getter/setters, payment calculation to CFO

---
+++ src/com/company/CFO.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class CFO {
+    double payment;
+    int numMonths;
+    double interestRate = .0038;
+    double principle;
+    double downPayment;
+
+    public double getDownPayment() {
+        return downPayment;
+    }
+
+    public void setDownPayment(double downPayment) {
+        this.downPayment = downPayment;
+    }
+
+    public double getPrinciple() {
+        return principle;
+    }
+
+    public void setPrinciple(double principle) {
+        this.principle = principle;
+    }
+
+    public int getNumMonths() {
+        return numMonths;
+    }
+
+    public void setNumMonths(int numMonths) {
+        this.numMonths = numMonths;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double calculatePayment(double principle, double interestRate, int numMonths){
+        payment = (principle * (1 +(interestRate/12) * numMonths))/ numMonths;
+        return payment;
+    }
+}
