00923b574a902f505d13d7f4e2391163d66ac1e0
elalimardani <58517842+elalimardani@users.noreply.github.com>
2020-09-23 21:25:19 -0400

    Added updatePrice method

--- src/com/company/DoI.java
+++ src/com/company/DoI.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 public class DoI {
 
-
     public static HashMap<String, Double> carList = new HashMap<String, Double>();
 
     public DoI(HashMap<String, Double> carList) {
         this.carList = carList;
     }
 
+    //to add a car to inventory
     public static void addCar(){
         System.out.println("In order to add a car to the list, you need a name and a price.\n Enter the car's name: ");
         Scanner scanner = new Scanner(System.in);
@@ -18,39 +18,52 @@ public class DoI {
         Double priceDouble = scanner.nextDouble();
         addCar(carString, priceDouble);
     }
-
-
+    
     public static void addCar(String carString, double priceDouble){
         carList.put(carString, priceDouble);
     }
 
-
+    //to remove a car from inventory
     public static void removeCar(){
         System.out.println("Please enter the name of the car you wish to remove. To view a list of the cars, type L , case-insensitive");
         Scanner scanner = new Scanner(System.in);
         String answer = scanner.nextLine();
 
-        if(answer == "l" || answer.toUpperCase() == "L"){
+        if(answer == "l"){
             printList();
-        } else if(carList.get(answer) != null){
-            System.out.println("Car found. Are you sure you want to remove this car with price of " + carList.get(answer) + "Y/N case-Sensitive");
-             if(scanner.next() == "Y"){
-                removeCar(answer);
+        } else if(carList.containsKey(answer)){
+            System.out.println("Car found. Are you sure you want to remove this car with price of " + carList.get(answer) + "? - y/n case-Sensitive");
+             if(scanner.next() == "y"){
+                 carList.remove(answer);
                  System.out.println("The car " + carList.get(answer) + " has been removed.");
              }
           }
-
+           else{
+            System.out.println("car not found");
+            }
         }
 
     //remove a car based on key (car name)
-    public static void removeCar(String carString){
+    /*public static void removeCar(String carString){
         carList.remove(carString);
-    }
+    }*/
 
     public static void updatePrice(){
-        System.out.println("updating car");
+        System.out.println("Please enter the name of the car you wish to update:\n");
+
+        Scanner scanner = new Scanner(System.in);
+        String carName = scanner.nextLine();
+
+        if(carList.containsKey(carName)){
+            System.out.println("The car " + carName + " is currently priced " + carList.get(carName) +
+                    ".\nEnter the new price to update:\n");
+            double newPrice = scanner.nextDouble();
+            carList.replace(carName, newPrice);
+            System.out.println("The car " + carName + " has been updated with the new price: " + carList.get(carName));
+        }
     }
 
+    //to print a list of cars in inventory
     public static void printList(){
         System.out.println("printing list");
         Set set = carList.entrySet();
@@ -61,7 +74,7 @@ public class DoI {
         }
     }
 
-
+    //to get the price of  a car
     public static void getDetails(){
         System.out.println("To receive details about a car, please enter the car's name");
         Scanner scanner = new Scanner(System.in);
@@ -71,5 +84,11 @@ public class DoI {
     public static void getDetails(String carName){
         System.out.printf("The price of this car is: " + carList.get(carName));
     }
+
+    //function to call from CFO to receive price of a car
+    public static double getPrice(String carName){
+        return carList.get(carName);
+    }
+
 }
 
